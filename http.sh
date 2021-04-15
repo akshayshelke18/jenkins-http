1788d4ea76682eb9b28dd7f9ebcc9eee2ddc9419
Akshay Shelke <56631503+akshayshelke18@users.noreply.github.com>
2021-04-15 18:29:38 +0530

    Create http.sh

---
+++ http.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo yum install -y httpd
+sudo systemctl start httpd && sudo sytemctl enable httpd
+
+echo "HELLO THIS IS JENKINS " > /var/www/html/index.html
+sudo systemctl restart httpd
+echo "Finish"
