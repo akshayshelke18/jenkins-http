013d6c01e4a0347ffc9194231030b04c4ab81074
Akshay Shelke <56631503+akshayshelke18@users.noreply.github.com>
2021-04-15 18:36:07 +0530

    Update http.sh

--- http.sh
+++ http.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install -y httpd
-sudo systemctl start httpd && sudo sytemctl enable httpd
+sudo systemctl start httpd && sudo systemctl enable httpd
 
 sudo echo "HELLO THIS IS JENKINS " > /var/www/html/index.html
 sudo systemctl restart httpd
